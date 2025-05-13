151759f81db24e4d638123084cb805458c4a5366
NadiaGiliberti <nadia.giliberti@stud.fhgr.ch>
2025-05-13 14:56:52 +0200

    update struktur aber noch nicht fertig (verknüpfungen etc fehlen)

--- etl/load.php
+++ etl/load.php
@@ -1,20 +1,22 @@
 <?php
  /*************************************************************
- * Kap. 13: Microcontroller -> DB 
+ * Kapitel 12: Website2DB > Schritt 3: Website -> DB
  * load.php
- * Daten als JSON-String vom vom MC serverseitig empfangen und in die Datenbank einfügen
+ * Daten als JSON-String vom Formular sender.html (später vom MC) serverseitig empfangen und Daten in die Datenbank einfügen
  * Datenbank-Verbindung
  * Ersetze $db_host, $db_name, $db_user, $db_pass durch deine eigenen Daten. 
  * Lade diese Datei NICHT auf GitHub
- * Beispiel: https://fiessling.ch/im4/13_MC2DB/load.php 
- * GitHub: https://github.com/Interaktive-Medien/im_physical_computing/blob/main/13_MC2DB/load.php
+ * Beispiel: https://fiessling.ch/im4/12_Website2DB/Schritt3_website_to_db/load.php 
+ * GitHub: https://github.com/Interaktive-Medien/im_physical_computing/blob/main/12_Website2DB/Schritt3_website_to_db/load.php
  *************************************************************/
+
 
 require_once("db_config.php");
 echo "This script receives HTTP POST messages and pushes their content into the database.";
 
 
 ###################################### connect to db
+
 try{
     $pdo = new PDO($dsn, $db_user, $db_pass, $options); 
     echo "</br> DB Verbindung ist erfolgreich";
@@ -31,6 +33,7 @@ catch(PDOException $e){
 
 $inputJSON = file_get_contents('php://input'); // JSON-Daten aus dem Body der Anfrage
 $input = json_decode($inputJSON, true); // Dekodieren der JSON-Daten in ein Array
+
 
 
 
@@ -58,11 +61,10 @@ $input = json_decode($inputJSON, true); // Dekodieren der JSON-Daten in ein Arra
 
 ###################################### receiving a post request from a HTML form, later from ESP32 C6
 
-$wert = $input["wert"];
+$wert = $input["wert"];         // Hol den Wert an der Stelle "wert" aus dem JS-Objekt (ehemals JSON-String)
 # insert new user into db
 $sql = "INSERT INTO sensordata (wert) VALUES (?)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$wert]);
-// $stmt->execute(["42"]);
 
 ?>
