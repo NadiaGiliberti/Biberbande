d1605bee2ae5f035811feee07dcd44e1dba6c84f
Silvana-art <silvana.waser@stud.fhgr.ch>
2025-05-16 00:22:15 +0200

    CSS und HTML Startseite
    Ich habe hauptsächlich am Css gearbeitet.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("blub");
+
+const heute = new Date();
+
+    const monat = heute.toLocaleDateString('de-DE', { month: 'long' });
+    const tag = heute.getDate();
+
+    document.getElementById('monat').textContent = monat;
+    document.getElementById('tag').textContent = tag;
